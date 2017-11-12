e51dec5519726bdeef249e5d0cf782a8595dd910
Bryan Simon <contact@brysi.xyz>
2017-11-12 16:58:19 +0100

    First commit, added all necessary files

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: simonb
+ * Date: 10/11/2017
+ * Time: 09:43
+ */
+// Redirect to the pages/home.php folder
+header('Locaiton: /tpe/home');
---
+++ pages/home.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: simonb
+ * Date: 10/11/2017
+ * Time: 09:45
+ */
+echo 'THIS IS A HOMEPAGE';
