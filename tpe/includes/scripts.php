ee5be308afeb8cf00ad1eedb29239f30fe9b72d7
SIMON Bryan <simonb>
2017-11-17 09:22:52 +0100

    Added vendors, includes

---
+++ tpe/includes/scripts.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: simonb
+ * Date: 17/11/2017
+ * Time: 08:26
+ */
+?>
+<!-- Bootstrap Core JavaScript -->
+<script src="/tpe/js/bootstrap.min.js"></script>
