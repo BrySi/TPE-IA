31d5f465bf28f4776dea27d6061c82c066ef91eb
SIMON Bryan <simonb>
2017-11-17 09:13:48 +0100

    Removed duplicate folders

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: simonb
- * Date: 10/11/2017
- * Time: 09:43
- */
-// Redirect to the pages/home.php folder
-header('Locaiton: /tpe/home');
--- pages/home.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: simonb
- * Date: 10/11/2017
- * Time: 09:45
- */
-echo 'THIS IS A HOMEPAGE';
