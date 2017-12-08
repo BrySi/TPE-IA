6f54e065e1dcdd5e4796086ff87bff654b17bcdb
Bryan SIMON <contact@brysi.xyz>
2017-12-08 09:51:21 +0100

    fixed an error in functions.php

--- tpe/includes/functions.php
+++ tpe/includes/functions.php
@@ -5,7 +5,7 @@
  * Date: 08/12/2017
  * Time: 09:26
  */
-public function get_lang($key) {
+function get_lang($key) {
     if($_COOKIE['lang'] = 'en') {
         $lang_file = json_decode(file_get_contents('../data/lang/en.json'));
     } else { // Par défaut on va chercher le fichier français
