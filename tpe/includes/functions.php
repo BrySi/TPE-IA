47afecdef07a65fd1d7cb9536e5c4cade53cecae
Bryan SIMON <contact@brysi.xyz>
2017-12-08 09:47:33 +0100

    Defined new language display method

--- tpe/includes/functions.php
+++ tpe/includes/functions.php
@@ -5,3 +5,11 @@
  * Date: 08/12/2017
  * Time: 09:26
  */
+public function get_lang($key) {
+    if($_COOKIE['lang'] = 'en') {
+        $lang_file = json_decode(file_get_contents('../data/lang/en.json'));
+    } else { // Par défaut on va chercher le fichier français
+        $lang_file = json_decode(file_get_contents('../data/lang/fr.json'));
+    }
+    return $lang_file->$key;
+}
